0124d003742bc3f86d8861c2d059b656b43bf03e
Otrin <oldan@web.de>
2014-06-13 18:48:13 +0200

    Somehow GIT via QT didn't push the Folder Signed-off-by: Otrin <oldan@web.de>

---
+++ lang/Translation_de.ts
@@ -0,0 +1,177 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>mainWindow</name>
+    <message>
+        <location filename="../src/gui/mainWindow.ui" line="14"/>
+        <source>GIF LZW Visualizer</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/mainWindow.ui" line="43"/>
+        <source>Bild Infos</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/mainWindow.ui" line="82"/>
+        <source>LZW Visualisierung</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/mainWindow.ui" line="101"/>
+        <location filename="../src/gui/mainWindow.ui" line="106"/>
+        <location filename="../src/gui/mainWindow.ui" line="111"/>
+        <location filename="../src/gui/mainWindow.ui" line="116"/>
+        <location filename="../src/gui/mainWindow.ui" line="121"/>
+        <source>Neue Zeile</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/mainWindow.ui" line="126"/>
+        <location filename="../src/gui/mainWindow.ui" line="131"/>
+        <source>Neue Spalte</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/mainWindow.ui" line="143"/>
+        <source>1 Schritt vor</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/mainWindow.ui" line="170"/>
+        <source>Automatisch</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/mainWindow.ui" line="180"/>
+        <source>Lokale/Globale Tabellen</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/mainWindow.ui" line="191"/>
+        <source>Globale Tabelle</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/mainWindow.ui" line="201"/>
+        <source>Lokale Tabelle</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/mainWindow.ui" line="220"/>
+        <source>Entropie-Vergleich</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/mainWindow.ui" line="228"/>
+        <source>Huffman</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/mainWindow.ui" line="235"/>
+        <source>LZW</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/mainWindow.ui" line="248"/>
+        <source>Lauflängen</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/mainWindow.ui" line="264"/>
+        <source>drawTest</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/mainWindow.ui" line="277"/>
+        <source>drawTestAnimated</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/mainWindow.ui" line="294"/>
+        <source>10fps</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/mainWindow.ui" line="301"/>
+        <source>30fps</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/mainWindow.ui" line="308"/>
+        <source>60fps</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/mainWindow.ui" line="331"/>
+        <source>Datei</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/mainWindow.ui" line="335"/>
+        <source>Speichern als...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/mainWindow.ui" line="345"/>
+        <source>Hilfe</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/mainWindow.ui" line="352"/>
+        <source>Language</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/mainWindow.ui" line="363"/>
+        <source>Datei öffnen...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/mainWindow.ui" line="366"/>
+        <source>Ctrl+N</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/mainWindow.ui" line="371"/>
+        <source>Anleitung</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/mainWindow.ui" line="376"/>
+        <source>Über...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/mainWindow.ui" line="387"/>
+        <source>German</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/mainWindow.ui" line="398"/>
+        <source>English</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/mainWindow.ui" line="403"/>
+        <source>GIF Bild</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/mainWindow.ui" line="406"/>
+        <source>Ctrl+S</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/mainWindow.ui" line="411"/>
+        <source>Lokale/Globale Tabellen Vergleichsbild</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/mainWindow.ui" line="414"/>
+        <source>Ctrl+G</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
