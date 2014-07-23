8a758fe3053d393821bf328e22767b99a01d0b99
ErikWeidmann <erikweidmann@web.de>
2014-07-23 23:56:27 +0200

    tried to fix some errors

--- src/entropy/huffman.h
+++ src/entropy/huffman.h
@@ -1,27 +1,10 @@
 #ifndef HUFFMAN_H
 #define HUFFMAN_H
-#include <map>
 
-class Huffman{
-private:
-    char *m_rawData, *m_compressedData, *m_codeTable;
-    int m_sizeOfRawData, m_sizeOfCompressedData, m_sizeOfCodeTable;
-    double m_compressionRate;
-    long m_compressionTime;
-    std::map<char, std::string> m_codeMap;
-    bool m_encode;
+class Huffman
+{
 public:
     Huffman();
-    char* encode(int sizeOfRawData, char *rawData);
-    char* decode(int sizeOfCompressedData, char *compressedData, int sizeOffCodeTable, char *codeTable);
-    char* getCodeTable();
-    int getSizeOfCodeTable();
-    int getSizeOfCompressedData();
-    int getSizeOfCompressedDataInBytes();
-    int getSizeOfRawData();
-    int getSizeOfRawDataInBytes();
-    std::string getStatistics();
-    void printStatistics();
 };
 
 #endif // HUFFMAN_H
